ceffd98f9903c6134524902566dca3c145bd9879
ltrel <79512656+ltrel@users.noreply.github.com>
2023-12-06 20:30:37 +1100

    Setup Radix UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { Theme, ThemePanel } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme appearance="dark">
+      <App />
+    </Theme>
   </React.StrictMode>,
 )
